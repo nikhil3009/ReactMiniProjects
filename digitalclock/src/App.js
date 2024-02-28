fb535ed8a60ff59b2cdacb285ed884eb2a331f36
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-02-28 18:19:43 -0500

    Digital Clock

---
+++ digitalclock/src/App.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import logo from './logo.svg';
+import './App.css';
+import DigitalClock from './DigitalClock';
+
+function App() {
+	return <DigitalClock />;
+}
+
+export default App;
