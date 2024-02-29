97e109d2b5249283437c995164ab42892206a1e2
nikhil3009 <38169100+nikhil3009@users.noreply.github.com>
2024-02-28 20:23:11 -0500

    Countdown for IPL

--- digitalclock/src/DigitalClock.jsx
+++ digitalclock/src/DigitalClock.jsx
@@ -3,36 +3,51 @@
 import React, { useState, useEffect } from 'react';
 
 const DigitalClock = () => {
-	const [time, setTime] = useState(new Date());
+	function calculateTimeLeft() {
+		const targetDate = new Date('March 22, 2024 20:00:00 GMT+0530');
+		const difference = targetDate - new Date();
+		let time = {};
+
+		if (difference > 0) {
+			time = {
+				days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+				hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+				minutes: Math.floor((difference / 1000 / 60) % 60),
+				seconds: Math.floor((difference / 1000) % 60),
+			};
+		}
+
+		return time;
+	}
+	const [time, setTime] = useState(calculateTimeLeft());
+
 	useEffect(() => {
-		const intervalId = setInterval(() => {
-			setTime(new Date());
+		const timer = setTimeout(() => {
+			setTime(calculateTimeLeft());
 		}, 1000);
 
-		return () => {
-			clearInterval(intervalId);
-		};
-	}, []);
+		return () => clearTimeout(timer);
+	});
 
-	function formatTime() {
-		let hours = time.getHours();
-		const minutes = time.getMinutes();
-		const seconds = time.getSeconds();
-		const meridean = hours >= 12 ? 'PM' : 'AM';
-		hours = hours % 12 || 12;
+	function formatCountdown(time) {
+		if (Object.keys(time).length === 0) {
+			return 'Enjoy the IPL Season!';
+		}
+		const { days, hours, minutes, seconds } = time;
 
-		return `${padZero(hours)}:${padZero(minutes)}:${padZero(
+		return `${days} Days ${padZero(hours)}hrs:${padZero(minutes)}mins:${padZero(
 			seconds
-		)} ${meridean}`;
+		)}secs`;
 	}
+
 	function padZero(number) {
-		return (number < 10 ? '0' : '') + number;
+		return number < 10 ? '0' + number : number;
 	}
 
 	return (
 		<div className='clock-container'>
 			<div className='clock'>
-				<span>{formatTime()}</span>
+				<span>{formatCountdown(time)}</span>
 			</div>
 		</div>
 	);
